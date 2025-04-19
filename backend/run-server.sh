59d0be6af8c7e81026351996138c55c51aa4175e
Dewix0 <danbat2006@gmail.com>
2025-04-19 11:52:20 +0300

    backend repo setup

---
+++ backend/run-server.sh
@@ -0,0 +1,6 @@
+pip install --no-cache-dir -r requirements.txt
+echo "Создание новых миграций"
+python manage.py makemigrations
+echo "Применение миграций"
+python manage.py migrate
+python manage.py runserver 0.0.0.0:8000
