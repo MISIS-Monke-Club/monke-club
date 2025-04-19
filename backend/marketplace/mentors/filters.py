a6e60f6a4cdea213f5cf69754e261f13a9151158
Dewix0 <danbat2006@gmail.com>
2025-04-19 22:09:51 +0300

    bombardiro filterino

--- backend/marketplace/mentors/filters.py
+++ backend/marketplace/mentors/filters.py
@@ -1,15 +1,15 @@
 import django_filters
+from django_filters.rest_framework import DjangoFilterBackend
 from marketplace.mentors.models import Mentor
+
+class CharInFilter(django_filters.BaseInFilter, django_filters.CharFilter):
+    pass
 
 
 class MentorFilters(django_filters.FilterSet):
     course = django_filters.NumberFilter(field_name="user__bio__course")
-    subject = django_filters.CharFilter(
-        field_name="subjects__name", lookup_expr="iexact"
-    )
-    services = django_filters.CharFilter(
-        field_name="services__name", lookup_expr="iexact"
-    )
+    subject = CharInFilter(field_name="subjects__name", lookup_expr="in")
+    services = CharInFilter(field_name="services__name", lookup_expr="in")
 
     class Meta:
         model = Mentor
