417c7bb5b3d4d4e7e17589a184fdce5a57871834
g0ldexperiencerequiem <pborisenko2006@yandex.ru>
2025-04-20 03:50:39 +0300

    added post for subg and servises

--- backend/marketplace/views.py
+++ backend/marketplace/views.py
@@ -10,10 +10,25 @@ class BombiniGusiniSubject(APIView):
         subject = Subject.objects.all()
         serializer = SubjectSerializer(subject, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
-
+    def post(self, request):
+        serializer = SubjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
 
 class BombiniGusiniService(APIView):
     def get(self, request):
         service = Service.objects.all()
         serializer = ServiceSerializer(service, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+    
+    def post(self, request):
+        serializer = ServiceSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+        
+
